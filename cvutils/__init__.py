940f0dc8e36c29a776f810678a946b289e8958ef
Francis Tyers <ftyers@prompsit.com>
2021-04-01 20:39:19 +0100

    missing init file

---
+++ cvutils/__init__.py
@@ -0,0 +1,8 @@
+import sys, os
+
+sys.path.append(os.path.dirname(__file__))
+
+from phonemiser import Phonemiser
+from segmenter import Segmenter  
+from validator import Validator
+from alphabet import Alphabet
