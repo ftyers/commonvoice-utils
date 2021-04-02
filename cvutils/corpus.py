4d2ea6e84919ce46be134975b0a3165682e1fd85
Francis Tyers <ftyers@prompsit.com>
2021-04-02 21:13:36 +0100

    add codes

--- cvutils/corpus.py
+++ cvutils/corpus.py
@@ -7,25 +7,32 @@ class Corpus:
 	'https://dumps.wikimedia.org/sahwiki/latest/sahwiki-latest-pages-articles.xml.bz2'
 	>>> c = Corpus('cv')
 	>>> c.target_segments()
-	['пӗрре','иккӗ','виҫҫӗ','тӑваттӑ','пиллӗк','улттӑ','ҫиччӗ','саккӑр','тӑххӑр','вуннӑ','ҫапла','ҫук']
+	['пӗрре', 'иккӗ', 'виҫҫӗ', 'тӑваттӑ', 'пиллӗк', 'улттӑ', 'ҫиччӗ', 'саккӑр', 'тӑххӑр', 'вуннӑ', 'ҫапла', 'ҫук']
+	>>> c = Corpus('ga-IE')
+	>>> c.dump_url()
+	'https://dumps.wikimedia.org/gawiki/latest/gawiki-latest-pages-articles.xml.bz2'
 	"""
 	def __init__(self, lang):
 		self.lang = lang
-		try:
-			self.load_data()
-		except FileNotFoundError:
-			print('[Corpus] Function not implemented')
+		self.load_data()
 
 	def load_data(self):
 		data_dir = os.path.abspath(os.path.dirname(__file__)) + '/data/'
-		fd = open(data_dir + self.lang + '/vocab.tsv')
-		self.small_vocab = [line.strip('\n') for line in fd.readlines()]
+		vocab_file = data_dir + self.lang + '/vocab.tsv'
+		self.small_vocab = []
+		if os.path.isfile(vocab_file):
+			fd = open(data_dir + self.lang + '/vocab.tsv')
+			self.small_vocab = [line.strip('\n') for line in fd.read().strip().split('\n')]
+		self.wikipedia_code = self.lang
+		wikipedia_file = data_dir + self.lang + '/wikipedia.txt'
+		if os.path.isfile(wikipedia_file):
+			self.wikipedia_code = open(wikipedia_file).read().strip()
 
 	def target_segments(self):
 		return self.small_vocab
 
 	def dump_url(self):
-		return 'https://dumps.wikimedia.org/%swiki/latest/%swiki-latest-pages-articles.xml.bz2' % (self.lang, self.lang)
+		return 'https://dumps.wikimedia.org/%swiki/latest/%swiki-latest-pages-articles.xml.bz2' % (self.wikipedia_code, self.wikipedia_code)
 
 
 if __name__ == "__main__":
