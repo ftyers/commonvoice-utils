eb1a1f749b1361b03be08bb8cbf06a05980c8a38
Francis Tyers <ftyers@prompsit.com>
2023-01-26 18:05:02 +0000

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="commonvoice-utils",
-    version="0.2.29",
+    version="0.2.30",
     description="Linguistic processing for languages in Common Voice",
     long_description=README,
     long_description_content_type="text/markdown",
